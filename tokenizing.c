f5adf667f3960cc14b8def8ef80b2b47e3ff81df
Shiyuji05 <153464960+Shiyuji05@users.noreply.github.com>
2023-12-10 17:25:50 -0500

    V2

---
+++ tokenizing.c
@@ -0,0 +1,59 @@
+//CPR final project V1
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 300
+#include "tokenizing.h"
+#include <stdio.h>
+#include <string.h>
+
+
+	/* Version 1 */
+/*void tokenizing(void) {
+	printf("*** Start of tokenizing Demo ***\n");
+	char words[BUFFER_SIZE];
+	char* nextWord = NULL;
+	int wordsCounter;
+	do {
+		printf("Type a few Words Separated by Space (q to quit):\n");
+		fgets(words, BUFFER_SIZE, stdin);
+		words[strlen(words) - 1] = '\0';
+		if (strcmp(words, "q") != 0) {
+			nextWord = strtok(words, " ");
+			wordsCounter = 1;
+			while (nextWord) {
+				printf("word #%d is \'%s\'\n", wordsCounter++, nextWord);
+				nextWord = strtok(NULL, " ");
+
+			}
+		}
+
+	} while (strcmp(words, "q") != '\0');
+	printf("*** End of tokenizing words Demo");
+}
+*/
+
+	/* Version 2 */
+	//>> insert here
+void tokenizing(void) {
+	printf("*** Start of Tokenizing Phrases Demo **\n");
+	char phrases[BUFFER_SIZE];
+	char* nextPhrase = NULL;
+	int phrasesCounter;
+	do {
+		printf("Type a few Phrases Seperated by comma(q - to quit):\n");
+		fgets(phrases, BUFFER_SIZE, stdin);
+		phrases[strlen(phrases) - 1] = '\0';
+		if ((strcmp(phrases, "q") != 0)) {
+			nextPhrase = strtok(phrases, ",");
+			phrasesCounter = 1;
+			while (nextPhrase) {
+				printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+				nextPhrase = strtok(NULL, ",");
+			}
+		}
+	} while (strcmp(phrases, "q") != 0);
+	printf("*** End of Tokenizing Phrases Demo ***\n\n");
+}
+	/* Version 3 */
+	//>> insert here
+
+
